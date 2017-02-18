a37af711b9242ac03eeb62e17b2f6b676bef5d67
bennpearson <bennpearson@gmail.com>
2017-02-18 09:02:06 +0000

    Wire up JSON textarea to state so it displays the JSON

--- src/containers/JsonWindow.js
+++ src/containers/JsonWindow.js
@@ -1,6 +1,20 @@
 import { connect } from 'react-redux'
 import JsonWindow from '../components/JsonWindow'
+import { change } from '../actions'
 
-const JsonWindowContainer = connect()(JsonWindow)
+const mapStateToProps = (state, ownProps) => {
+  return { question: state.question }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onChange: (value) => dispatch(change(value))
+  }
+}
+
+const JsonWindowContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(JsonWindow)
 
 export default JsonWindowContainer
