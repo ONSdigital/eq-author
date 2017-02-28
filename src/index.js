792453681d969495492317b618ca2e288b31d08b
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-02-28 11:11:56 +0000

    Add boilerplate required to save and load store.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import authorApp from './reducers'
+
+import { createMiddleware, reducer, createLoader } from 'redux-storage'
+import createEngine from 'redux-storage-engine-localstorage'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import App from './components/App'
 import './index.css'
 
-let store = createStore(
-  authorApp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const storageReducer = reducer(authorApp)
+const storageEngine = createEngine('eq-authoring-prototype-storage-key')
+const middleware = createMiddleware(storageEngine)
+
+let store = createStore(storageReducer, composeWithDevTools(
+  applyMiddleware(middleware)
+))
+
+const load = createLoader(storageEngine);
+load(store);
 
 ReactDOM.render(
   <Provider store={store}>
