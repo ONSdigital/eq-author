48bceb233f5f56a877cda62a8fcfd677436e0c7b
Hamish Taplin <hello@hamishtaplin.com>
2017-02-28 15:55:31 +0000

    removed values from initial state

--- src/reducers/question.js
+++ src/reducers/question.js
@@ -2,32 +2,25 @@ import {merge} from 'lodash'
 import { CHANGE_QUESTION, CHANGE_RESPONSE, REMOVE_OPTION, ADD_OPTION } from '../actions'
 
 const initialState = {
-  "description": "Lorem ipsum",
-  "id": "question-1",
-  "number": "1",
-  "title": "Which side of the force are you on?",
-  "type": "General",
+  "description": "",
+  "id": "",
+  "number": "",
+  "title": "",
+  "type": "",
   "guidance": {
-    "title": "Include",
-    "text": "Maecenas faucibus mollis interdum."
+    "title": "",
+    "text": ""
   },
   "answers": [{
-        "id": "permanent-or-family-home-answer",
+        "id": "",
         "description": "",
         "mandatory": true,
-        "guidance": "<p>For most people, their permanent home will be the address where they spend the most time.</p>",
-        "options": [{
-            "label": "Yes",
-            "value": "Yes"
-        }, {
-            "label": "No",
-            "value": "No",
-            "description": "For example this is a second address or holiday home"
-        }],
-        "type": "Radio",
+        "guidance": "",
+        "options": [],
+        "type": "",
         "validation": {
             "messages": {
-                "MANDATORY": "Please select an answer to continue"
+                "MANDATORY": ""
             }
         }
     }],
