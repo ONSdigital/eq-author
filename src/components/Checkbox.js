6b25d0ab74b40f4e64e4ea3a4f9f794a9d17c19f
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-03-02 13:09:01 +0000

    Remove onChange from Checkbox component.

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 
 export default class Checkbox extends Component {
   render() {
-    const { checked, onChange, ...otherProps } = this.props
+    const { checked, ...otherProps } = this.props
 
     return (
-      <input type="checkbox" checked={checked} onChange={onChange} {...otherProps} />
+      <input type="checkbox" checked={checked} {...otherProps} />
     )
   }
 }
