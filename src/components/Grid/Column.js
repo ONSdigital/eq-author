fb01b85315e3f4e2eec3b250e7379261a8526f27
Hamish Taplin <hello@hamishtaplin.com>
2017-04-12 15:43:38 +0100

    removed gutters on first and last grid column

--- src/components/Grid/Column.js
+++ src/components/Grid/Column.js
@@ -5,6 +5,12 @@ const Column = styled.div`
   flex: 1 1 auto;
   padding: 0 0.5em;
   max-width: ${({cols}) => cols / 12 * 100}%;
+  &:first-child {
+    padding-left: 0;
+  }
+  &:last-child {
+    padding-right: 0;
+  }
 `
 
 export default Column
