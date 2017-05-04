6e338361a65d95d811a193753b6bb94a59cc1f62
Emilio Ward <emilioward@hotmail.co.uk>
2017-05-04 09:55:56 +0100

    Added 404 routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {Route} from 'react-router';
 import {ConnectedRouter} from 'react-router-redux';
 import routes from 'routes';
+import {Switch} from 'react-router-dom';
+
+import SurveyPage from 'containers/Survey';
+import CreateSurvey from 'containers/CreateSurvey';
+import DesignSurvey from 'containers/DesignSurvey';
+import NotFound from 'pages/NotFound';
 
 const RouteWithSubRoutes = route => (
   <Route
@@ -10,10 +16,11 @@ const RouteWithSubRoutes = route => (
     render={props => <route.component {...props} routes={route.routes} />}
   />
 );
+
 export default ({history}) => (
   <ConnectedRouter history={history}>
-    <div>
+    <Switch>
       {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
-    </div>
+    </Switch>
   </ConnectedRouter>
 );
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import {FullPageLayout} from 'layouts';
+
+const Centered = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const NotFound = props => {
   return (
     <FullPageLayout>
-      <h1>Page not found</h1>
+      <Centered>
+        <h1>404 Error - Page not found</h1>
+      </Centered>
     </FullPageLayout>
   );
 };
