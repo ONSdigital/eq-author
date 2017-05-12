eed5f6ac226a1986c7feaf5ea8429f154e15cc0f
WickyNilliams <WickyNilliams@MBA>
2017-05-12 11:34:47 +0100

    Extract readFileAsJson util

--- src/containers/Survey.js
+++ src/containers/Survey.js
@@ -1,8 +1,9 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import { loadSurvey } from "actions/survey";
-import SurveyPage from "pages/Survey";
+import {loadSurvey} from 'actions/survey';
+import SurveyPage from 'pages/Survey';
+import readFileAsJSON from "utils/readFileAsJson";
 
 const mapStateToProps = (state, ownProps) => {
   return {};
@@ -10,17 +11,17 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, { history }) => {
   return {
-    onFileSelected: e => {
+    onFileSelected(e) {
       const file = e.target.files[0];
-      if (file) {
-        const fileReader = new FileReader();
-        fileReader.onload = f => {
-          const data = JSON.parse(f.target.result);
-          dispatch(loadSurvey(data));
-          history.push("/create");
-        };
-        fileReader.readAsText(file);
+
+      if (!file) {
+        return;
       }
+
+      readFileAsJSON(file).then(data => {
+        dispatch(loadSurvey(data));
+        history.push('/create');
+      });
     },
   };
 };
