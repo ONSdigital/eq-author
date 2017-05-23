2bd949852faab404773cbc42351c2e2b937e8966
WickyNilliams <wicky@nillia.ms>
2017-05-23 16:52:44 +0100

    add some error checking to smokiest script

--- scripts/run_smoketests.sh
+++ scripts/run_smoketests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -e
 
 # Run the server
 ./node_modules/.bin/serve --port 3000 &
 pid=$!
+
+if ! ps -p $pid > /dev/null
+then
+   echo "Couldn't start server";
+   exit;
+fi
+
 
 # Wait for server to start listening
 sleep 3
