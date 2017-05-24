aa4ca85fd3af5f046ac4247a14937ff9b4734e2a
WickyNilliams <wicky@nillia.ms>
2017-05-24 12:03:48 +0100

    please work, i beg you

--- features/StepDefinitions/definition.js
+++ features/StepDefinitions/definition.js
@@ -1,4 +1,5 @@
 /* global browser */
+/* eslint-disable no-console */
 const expect = require("chai").expect;
 
 module.exports = function () {
@@ -7,8 +8,9 @@ module.exports = function () {
   });
 
   this.Then(/no errors are logged/, () => {
+    console.log("foo");
     const logs = browser.log("browser");
-    console.log(logs); // eslint-disable-line
+    console.log(logs);
     // const errors = logs.value.filter(log => log.level === "SEVERE");
 
     expect(logs.value.length).to.be.eql(0);
