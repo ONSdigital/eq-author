610fe1f3eeb6791d146e432fe5fe23b62a48382f
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-06-01 17:09:11 +0100

    Use Jasmine for tests.

---
+++ features/smoketest-spec.js
@@ -0,0 +1,15 @@
+describe("eQ Author", () => {
+  describe("navigate to the Homepage", () => {
+    beforeEach(() => {
+      browser.url("http://localhost:3000/");
+    });
+
+    it("displays page title", () => {
+      expect(browser.getTitle()).toEqual("eQ Author Prototype");
+    });
+
+    it("displays option buttons", () => {
+      expect(browser.source().value).toContain("Select to begin");
+    });
+  });
+});
--- features/smoketest.feature
+++
@@ -1,8 +0,0 @@
-Feature: Author application is alive
-  As an eQ Author
-  I should be able to browse to the Author application
-  So that I can author a new survey
-
-Scenario: Homepage is responding
-  Given I navigate to "http://localhost:3000"
-  Then the page title should be "eQ Author Prototype"
