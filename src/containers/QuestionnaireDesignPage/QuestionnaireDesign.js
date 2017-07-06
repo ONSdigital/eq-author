7c271e1d052daf4fa7f9e6dcf0f14faebc7d2d96
Benn Pearson <bennpearson@gmail.com>
2017-07-06 13:55:35 +0100

    Question properties (#147)
    * added questionnaire properties
    
    * fed review
    
    * lowercase amend
    
    * updated tests

--- src/containers/QuestionnaireDesignPage/QuestionnaireDesign.js
+++ src/containers/QuestionnaireDesignPage/QuestionnaireDesign.js
@@ -2,24 +2,42 @@ import React from "react";
 import PropTypes from "prop-types";
 import CustomPropTypes from "custom-prop-types";
 import BaseLayout from "components/BaseLayout";
+import { Grid, Column } from "components/Grid";
+import { PropertyPane, PropertyPaneTitle } from "components/PropertyPane";
+import QuestionProperties from "components/QuestionProperties";
 
-const QuestionnaireDesign = ({ loading, questionnaire }) => {
+const QuestionnaireDesign = ({ loading, questionnaire, question }) => {
   if (loading) {
     return <div>Loading...</div>;
   }
-
+  const handleSubmit = () => {};
   return (
     <BaseLayout
       breadcrumb={{ path: window.location.href, title: questionnaire.title }}
     >
-      Design goes here
+      <Grid align="top">
+        <Column cols={3} gutters={false}>
+          Sidebar
+        </Column>
+        <Column gutters={false}>Preview goes here</Column>
+        <Column cols={2} gutters={false}>
+          <PropertyPane>
+            <PropertyPaneTitle>Question properties</PropertyPaneTitle>
+            <QuestionProperties
+              question={{ type: "General" }}
+              onSubmit={handleSubmit}
+            />
+          </PropertyPane>
+        </Column>
+      </Grid>
     </BaseLayout>
   );
 };
 
 QuestionnaireDesign.propTypes = {
   loading: PropTypes.bool.isRequired,
-  questionnaire: CustomPropTypes.questionnaire
+  questionnaire: CustomPropTypes.questionnaire,
+  question: CustomPropTypes.question
 };
 
 export default QuestionnaireDesign;
