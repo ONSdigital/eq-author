d1e95e388fc29c771bf0f6fe8349799f09f8cff5
Hamish Taplin <hello@hamishtaplin.com>
2017-07-10 16:11:07 +0100

    remove sidebar/treemenu

--- src/components/TreeMenu/index.js
+++
@@ -1,5 +0,0 @@
-export { default as TreeMenu } from "./TreeMenu";
-export { default as TreeNode } from "./TreeNode";
-export { default as TreeNodeChildren } from "./TreeNodeChildren";
-export { default as TreeNodeLabel } from "./TreeNodeLabel";
-export { default as TreeMenuContainer } from "./TreeMenuContainer";
