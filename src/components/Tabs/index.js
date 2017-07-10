3b598be99e8a43d3a30948cbb65e3ea410b4a759
Hamish Taplin <hello@hamishtaplin.com>
2017-07-10 16:34:37 +0100

    remove Tabs and Tables

--- src/components/Tabs/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Tabs } from "./Tabs";
-export { default as TabPanel } from "./TabPanel";
-export { default as TabList } from "./TabList";
-export { default as TabTitle } from "./TabTitle";
