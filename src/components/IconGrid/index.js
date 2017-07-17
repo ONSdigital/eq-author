b8daca08808f3b671d053e427051d7d574424492
Benn Pearson <bennpearson@gmail.com>
2017-07-17 16:29:18 +0100

    added IconGrid

---
+++ src/components/IconGrid/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Grid, Column } from "components/Grid";
+
+const IconGrid = () => {
+  return (
+    <div>
+      <Grid>
+        <Column cols={4}>sdfds</Column>
+        <Column cols={4}>sdfds</Column>
+        <Column cols={4}>sdfds</Column>
+      </Grid>
+      <Grid>
+        <Column cols={4}>sdfds</Column>
+        <Column cols={4}>sdfds</Column>
+        <Column cols={4}>sdfds</Column>
+      </Grid>
+    </div>
+  );
+};
+
+export default IconGrid;
---
+++ src/components/IconGrid/story.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import IconGrid from "./index";
+
+storiesOf("IconGrid", module).add("Default", () => <IconGrid />);
