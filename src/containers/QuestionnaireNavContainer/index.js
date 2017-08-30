15d03b7ac2af77fa25f1457278d28505a177bb7e
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-08-30 15:29:53 +0100

    Removing unneccessary code.

--- src/containers/QuestionnaireNavContainer/index.js
+++ src/containers/QuestionnaireNavContainer/index.js
@@ -1,24 +1,12 @@
-import { graphql, compose } from "react-apollo";
+import { compose } from "react-apollo";
 import { withRouter } from "react-router";
 
-import getQuestionnaireQuery from "graphql/getQuestionnaire.graphql";
+import withQuestionnaire from "../Enhancers/withQuestionnaire";
 import withCreatePage from "../Enhancers/withCreatePage";
 import withDeletePage from "../Enhancers/withDeletePage";
 
 import QuestionnaireNav from "components/QuestionnaireNav";
 import withCreateSection from "../Enhancers/withCreateSection";
-
-export const mapResultsToProps = ({ data }) => {
-  return {
-    questionnaire: data.questionnaire,
-    loading: data.loading
-  };
-};
-
-export const withQuestionnaire = graphql(getQuestionnaireQuery, {
-  props: mapResultsToProps,
-  options: props => ({ variables: { id: props.questionnaireId } })
-});
 
 export default compose(
   withRouter,
