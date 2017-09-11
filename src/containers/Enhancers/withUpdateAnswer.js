8f983410eb31e91d274e8448b6de8ec1d8d9a5cf
WickyNilliams <wicky@nillia.ms>
2017-09-11 11:30:39 +0100

    add tests for withUpdateAnswer

--- src/containers/Enhancers/withUpdateAnswer.js
+++ src/containers/Enhancers/withUpdateAnswer.js
@@ -1,8 +1,10 @@
 import updateAnswerMutation from "graphql/updateAnswer.graphql";
 import { graphql } from "react-apollo";
 
+export const mapMutateToProps = ({ mutate }) => ({
+  onUpdateAnswer: answer => mutate({ variables: answer })
+});
+
 export default graphql(updateAnswerMutation, {
-  props: ({ mutate }) => ({
-    onUpdateAnswer: answer => mutate({ variables: answer })
-  })
+  props: mapMutateToProps
 });
