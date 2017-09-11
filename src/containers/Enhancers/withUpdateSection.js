eaaf16930ff4c0a4e451ffee6e46d7f5582a42c0
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-11 11:04:35 +0100

    Add test for withUpdateSection enhancer.

--- src/containers/Enhancers/withUpdateSection.js
+++ src/containers/Enhancers/withUpdateSection.js
@@ -1,8 +1,10 @@
 import updateSectionMutation from "graphql/updateSection.graphql";
 import { graphql } from "react-apollo";
 
+export const mapMutateToProps = ({ mutate }) => ({
+  onUpdateSection: section => mutate({ variables: section })
+});
+
 export default graphql(updateSectionMutation, {
-  props: ({ mutate }) => ({
-    onUpdateSection: section => mutate({ variables: section })
-  })
+  props: mapMutateToProps
 });
