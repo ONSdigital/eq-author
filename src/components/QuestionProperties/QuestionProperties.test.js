d796ce77ddb5ad656211ab8a053e3aa1fee91ec5
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-14 14:30:30 +0100

    Remove test which no longer applies.

--- src/components/QuestionProperties/QuestionProperties.test.js
+++ src/components/QuestionProperties/QuestionProperties.test.js
@@ -12,15 +12,12 @@ const handleSubmit = jest.fn();
 const handleChange = jest.fn();
 const handleBlur = jest.fn();
 
-const question = jest.fn();
-
 describe("QuestionProperties", () => {
   beforeEach(() => {
     wrapper = shallow(
       <QuestionProperties
         onUpdate={handleUpdate}
         loading={false}
-        question={question}
         onSubmit={handleSubmit}
         onChange={handleChange}
         onBlur={handleBlur}
@@ -45,10 +42,5 @@ describe("QuestionProperties", () => {
   it("should handle blur event", () => {
     wrapper.find(Select).simulate("blur");
     expect(handleBlur).toHaveBeenCalled();
-  });
-
-  it("should pass question type to select", () => {
-    const selectValue = wrapper.find(Select).prop("defaultValue");
-    expect(selectValue).toBeUndefined();
   });
 });
