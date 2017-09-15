8847496a2fa5a929aeec971148780b27e57ac636
WickyNilliams <wicky@nillia.ms>
2017-09-15 14:50:34 +0100

    improve MultipleChoiceAnswer story

--- src/components/Answers/MultipleChoiceAnswer/MultipleChoiceAnswer.story.js
+++ src/components/Answers/MultipleChoiceAnswer/MultipleChoiceAnswer.story.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { merge, reject, set } from "lodash";
-import CheckboxAnswer from "./index";
+import MultipleChoiceAnswer from "./index";
+import { CHECKBOX, RADIO } from "constants/answer-types";
 
 const options = [
   {
@@ -19,48 +21,54 @@ const options = [
   }
 ];
 
-class CheckboxAnswerWrapper extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      answer: {
-        id: 0,
-        options
-      }
-    };
-    this.nextId = 1;
-  }
-
-  handleChange = ({ name, value }) => {
-    this.setState(set(merge({}, this.state), name, value));
+class MultipleChoiceAnswerWrapper extends React.Component {
+  static propTypes = {
+    type: PropTypes.string
   };
 
+  state = {
+    answer: {
+      id: 0,
+      options
+    }
+  };
+
+  nextId = 1;
+
   handleAddOption = () => {
+    const { answer } = this.state;
     const newOption = {
       ...options[0],
       id: ++this.nextId
     };
 
-    const newState = merge({}, this.state);
-    newState.answer.options.push(newOption);
+    const newState = {
+      answer: {
+        ...answer,
+        options: answer.options.concat(newOption)
+      }
+    };
+
     this.setState(newState);
   };
 
-  handleDeleteOption = optionId => {
-    const newState = merge({}, this.state);
-    newState.answer.options = reject(newState.answer.options, {
-      id: optionId
-    });
+  handleDeleteOption = id => {
+    const { answer } = this.state;
+
+    const newState = {
+      answer: {
+        ...answer,
+        options: reject(answer.options, { id })
+      }
+    };
+
     this.setState(newState);
   };
-
-  handleAddOther() {
-    alert("Add other answer");
-  }
 
   render() {
     return (
-      <CheckboxAnswer
+      <MultipleChoiceAnswer
+        type={this.props.type}
         answer={this.state.answer}
         onUpdateOption={action("update option")}
         onUpdate={action("update")}
@@ -71,6 +79,6 @@ class CheckboxAnswerWrapper extends React.Component {
   }
 }
 
-storiesOf("AnswerTypes/CheckboxAnswer", module).add("Default", () =>
-  <CheckboxAnswerWrapper />
-);
+storiesOf("AnswerTypes/MultipleChoiceAnswer", module)
+  .add(RADIO, () => <MultipleChoiceAnswerWrapper type={RADIO} />)
+  .add(CHECKBOX, () => <MultipleChoiceAnswerWrapper type={CHECKBOX} />);
