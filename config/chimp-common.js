9239c439bbd85d372044daf32c56e73dce28990f
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-15 10:15:07 +0100

    Revert to default chimp selenium and chromedriver versions.

--- config/chimp-common.js
+++ config/chimp-common.js
@@ -13,22 +13,6 @@ module.exports = {
     },
     logLevel: "error"
   },
-  seleniumStandaloneOptions: {
-    // check for more recent versions of selenium here:
-    // http://selenium-release.storage.googleapis.com/index.html
-    version: "3.5.3",
-    // http://selenium-release.storage.googleapis.com/3.5/selenium-server-standalone-3.5.3.jar
-    baseURL: "https://selenium-release.storage.googleapis.com",
-    drivers: {
-      chrome: {
-        // check for more recent versions of chrome driver here:
-        // http://chromedriver.storage.googleapis.com/index.html
-        version: "2.32",
-        arch: process.arch,
-        baseURL: "https://chromedriver.storage.googleapis.com"
-      }
-    }
-  },
   jasmineConfig: {
     stopSpecOnExpectationFailure: true
   }
