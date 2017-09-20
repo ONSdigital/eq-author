1603768ed0c03e5899196f3d8346064acc649d14
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-20 09:41:15 +0100

    Remove definitions of component from inside render function.

--- src/containers/QuestionnairesPage/CommentsButton.js
+++ src/containers/QuestionnairesPage/CommentsButton.js
@@ -6,47 +6,43 @@ import Button from "components/Button";
 import CommentsSvg from "./commentsIcon.svg";
 import UnreadCommentsSvg from "./commentsUnreadNotification.svg";
 
-const CommentsButton = ({ hasUnread }) => {
-  const handleClick = () => {
-    alert("Not implemented yet.");
-  };
-
-  const StyledSpan = styled.span`
-    position: relative;
-    top: 2px;
-  `;
-
-  const StyledButton = styled(Button)`
-    width: 18px;
-    height: 18px;
-    padding: .5em;
-    background: url(${CommentsSvg}) no-repeat;
-    background-size: contain;
-    border: 0;
-    
-    &:hover { 
-      opacity: 0.7;
-      transition: opacity 150ms ease-in;
-    }
-  `;
-
-  const UnreadCommentsNotifier = styled.span`
-    background: url(${UnreadCommentsSvg}) no-repeat;
-    width: 8px;
-    height: 8px;
-    position: absolute;
-    top: -2px;
-    right: -4px;
-  `;
-
-  return (
-    <StyledSpan>
-      <StyledButton title={"Click to see comments"} onClick={handleClick} />
-      {hasUnread &&
-        <UnreadCommentsNotifier title={"You have unread comments"} />}
-    </StyledSpan>
-  );
+const handleClick = () => {
+  alert("Not implemented yet.");
 };
+
+const StyledSpan = styled.span`
+  position: relative;
+  top: 2px;
+`;
+
+const StyledButton = styled(Button)`
+  width: 18px;
+  height: 18px;
+  padding: .5em;
+  background: url(${CommentsSvg}) no-repeat;
+  background-size: contain;
+  border: 0;
+  
+  &:hover { 
+    opacity: 0.7;
+    transition: opacity 150ms ease-in;
+  }
+`;
+
+const UnreadCommentsNotifier = styled.span`
+  background: url(${UnreadCommentsSvg}) no-repeat;
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  top: -2px;
+  right: -4px;
+`;
+
+const CommentsButton = ({ hasUnread }) =>
+  <StyledSpan>
+    <StyledButton title={"Click to see comments"} onClick={handleClick} />
+    {hasUnread && <UnreadCommentsNotifier title={"You have unread comments"} />}
+  </StyledSpan>;
 
 CommentsButton.propTypes = {
   hasUnread: PropTypes.bool.isRequired
