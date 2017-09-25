4a7759a0b81896a9d7bd59e141b0ea200da58da7
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-09-25 09:20:11 +0100

    Change width of number input to match design.

--- src/components/Answers/NumberAnswer/index.js
+++ src/components/Answers/NumberAnswer/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import BasicAnswer from "components/Answers/BasicAnswer";
 import DummyTextInput from "components/Answers/Dummy/TextInput";
+import styled from "styled-components";
+
+const DummyNumberInput = styled(DummyTextInput)`
+  width: 40%;
+`;
 
 const NumberAnswer = props =>
   <BasicAnswer {...props}>
     <div>
-      <DummyTextInput />
+      <DummyNumberInput />
     </div>
   </BasicAnswer>;
 
