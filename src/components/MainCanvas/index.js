7bacd6729097699d1d8074bfa946a832b7269207
Hamish Taplin <hello@hamishtaplin.com>
2017-09-27 14:23:18 +0100

    update snapshots

--- src/components/MainCanvas/index.js
+++ src/components/MainCanvas/index.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const Canvas = styled.div`
+const MainCanvas = styled.div`
   width: 100%;
   max-width: 55em;
   margin: 2em auto 1em;
 `;
 
-export default Canvas;
+export default MainCanvas;
