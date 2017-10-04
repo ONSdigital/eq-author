9b7e1de5cd884182d9b182be5397b5058b861e6b
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-10-04 10:26:11 +0100

    Fix error observed on admin page when running with the mockAPI.

--- src/apollo/createMockNetworkInterface.js
+++ src/apollo/createMockNetworkInterface.js
@@ -1,4 +1,15 @@
 import schema from "eq-author-graphql-schema";
+import { makeExecutableSchema } from "graphql-tools";
 const { MockNetworkInterface } = require("eq-author-mock-api");
+const { GraphQLDate } = require("graphql-iso-date");
 
-export default (mocks = {}) => new MockNetworkInterface(schema, mocks);
+export default (mocks = {}) => {
+  const executableSchema = makeExecutableSchema({
+    typeDefs: schema,
+    resolvers: {
+      Date: GraphQLDate
+    }
+  });
+
+  return new MockNetworkInterface(executableSchema, mocks);
+};
