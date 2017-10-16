664c83b2528e0293db3acdd9291a89c70180e80d
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-10-16 13:32:32 +0100

    Add test to satisfy codecov.

--- src/components/IconLink/IconLink.test.js
+++ src/components/IconLink/IconLink.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import "styled-components-test-utils/lib/jest";
 
 import IconLink, { StyledLink } from "components/IconLink";
 import iconTest from "./icon-test.svg";
@@ -26,5 +27,22 @@ describe("components/IconLink", function() {
   it("will handleClick", function() {
     wrapper.find(StyledLink).simulate("click");
     expect(handleClick).toHaveBeenCalled();
+  });
+});
+
+describe("components/IconLink/StyledLink", () => {
+  beforeEach(() => {
+    wrapper = shallow(<StyledLink icon={iconTest} />);
+  });
+
+  it("will render StyledLink", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should have an icon", () => {
+    expect(wrapper).toHaveStyleRule(
+      "background",
+      "transparent url(icon-test.svg) no-repeat center"
+    );
   });
 });
