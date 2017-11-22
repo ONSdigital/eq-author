a0b0bccef8c945c5e344377db945bca76c082dc7
Samiwel Thomas <samiwel.thomas@gmail.com>
2017-11-22 14:45:34 +0000

    Add custom 404 page design.

--- src/containers/NotFoundPage/index.js
+++ src/containers/NotFoundPage/index.js
@@ -1,14 +1,40 @@
 import React from "react";
 
 import BaseLayout from "components/BaseLayout";
+import { Grid, Column } from "components/Grid";
+import Link from "components/Link";
+import brokenPencil from "./broken-pencil.min.svg";
+import styled from "styled-components";
+
+const CenteredPane = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 6em;
+`;
+
+const Pencil = styled.div`
+  background: url(${brokenPencil}) no-repeat top left;
+  background-size: contain;
+  width: 397px;
+  height: 135px;
+`;
+
+const pageTitle = "Page not found";
 
 const NotFound = props => {
   return (
-    <BaseLayout
-      title="404—Page not found"
-      docTitle="Page not found"
-      hasNav={false}
-    />
+    <BaseLayout docTitle={pageTitle} hasNav={false}>
+      <Grid>
+        <Column cols={6} offset={3}>
+          <CenteredPane>
+            <Pencil />
+            <p>404 - Sorry, the page you were looking for was not found</p>
+            <Link href="/">Back to home</Link>
+          </CenteredPane>
+        </Column>
+      </Grid>
+    </BaseLayout>
   );
 };
 
