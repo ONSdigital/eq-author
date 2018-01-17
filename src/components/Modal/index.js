028382ecc4abfe5bc1e47bcc3348ada0fb8037c6
Samiwel Thomas <samiwel.thomas@gmail.com>
2018-01-17 14:56:39 +0000

    Add animation to modal.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactModal from "react-modal";
-import styled from "styled-components";
+import styled, { injectGlobal } from "styled-components";
 import PropTypes from "prop-types";
 
 const reactModalStyleOverride = {
@@ -8,6 +8,42 @@ const reactModalStyleOverride = {
     backgroundColor: "rgba(0, 0, 0, 0.5)"
   }
 };
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+.ReactModalPortal > div {
+    opacity: 0;
+}
+
+.ReactModalPortal .ReactModal__Overlay {
+  transition: opacity 200ms;
+
+  &--after-open {
+    opacity: 1;
+  }
+
+  &--before-close {
+      opacity: 0;
+  }
+}
+
+.ReactModalPortal .ReactModal__Content {
+  transform: translateX(-50%) translateY(-50%) scale(0.8);
+  transform-origin: center center;
+  transition: all 200ms ease-in 200ms;
+  opacity: 0;
+
+  &--after-open {
+    transform: translateX(-50%) translateY(-50%) scale(1);
+    opacity: 1;
+  }
+
+  &--before-close {
+    transform: translateX(-50%) translateY(-50%) scale(0.8);
+    opacity: 0;
+  }
+}
+`;
 
 const StyledModal = styled(ReactModal)`
   position: absolute;
@@ -35,6 +71,7 @@ class Modal extends React.Component {
         onRequestClose={onClose}
         shouldCloseOnOverlayClick
         style={reactModalStyleOverride}
+        closeTimeoutMS={400}
       >
         {children}
       </StyledModal>
