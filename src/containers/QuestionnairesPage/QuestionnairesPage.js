e8416816f7c342f6512ab752eea87f4e1e5eb7e2
WickyNilliams <wicky@nillia.ms>
2018-02-06 17:32:49 +0000

    integrate QuestionnaireSettingsModal into QuestionnairesPage

--- src/containers/QuestionnairesPage/QuestionnairesPage.js
+++ src/containers/QuestionnairesPage/QuestionnairesPage.js
@@ -5,9 +5,10 @@ import CustomPropTypes from "custom-prop-types";
 import BaseLayout from "components/BaseLayout";
 import { CenteredPanel } from "components/Panel";
 import ButtonGroup from "components/ButtonGroup";
-import LinkButton from "components/LinkButton";
+import Button from "components/Button";
 import QuestionnairesTable from "./QuestionnairesTable";
 import MainCanvas from "components/MainCanvas";
+import QuestionnaireSettingsModal from "components/QuestionnaireSettingsModal";
 
 const StyledButtonGroup = styled(ButtonGroup)`
   margin: 0 0 1em;
@@ -17,31 +18,52 @@ const StyledCenteredPanel = styled(CenteredPanel)`
   padding: 0;
 `;
 
-const Questionnaires = props => {
-  const title = "Your Questionnaires";
-  return (
-    <BaseLayout title={title} docTitle={title}>
-      <MainCanvas>
-        <StyledButtonGroup horizontal>
-          <LinkButton
-            to="/questionnaire/create"
-            id="btn-create-questionnaire"
-            primary
-          >
-            Create
-          </LinkButton>
-        </StyledButtonGroup>
-        <StyledCenteredPanel>
-          {!props.loading && <QuestionnairesTable {...props} />}
-        </StyledCenteredPanel>
-      </MainCanvas>
-    </BaseLayout>
-  );
-};
+class QuestionnairesPage extends React.Component {
+  state = {
+    isModalOpen: false
+  };
 
-Questionnaires.propTypes = {
+  handleModalOpen = () => this.setState({ isModalOpen: true });
+  handleModalClose = () => this.setState({ isModalOpen: false });
+
+  render() {
+    const { createQuestionnaire, loading, ...props } = this.props;
+
+    return (
+      <BaseLayout
+        title={"Your Questionnaires"}
+        docTitle={"Your Questionnaires"}
+      >
+        <MainCanvas>
+          <StyledButtonGroup horizontal>
+            <Button
+              onClick={this.handleModalOpen}
+              id="btn-create-questionnaire"
+              primary
+            >
+              Create
+            </Button>
+            <QuestionnaireSettingsModal
+              isOpen={this.state.isModalOpen}
+              onClose={this.handleModalClose}
+              onSubmit={createQuestionnaire}
+              confirmText="Create"
+            />
+          </StyledButtonGroup>
+          <StyledCenteredPanel>
+            {!loading && <QuestionnairesTable {...props} />}
+          </StyledCenteredPanel>
+        </MainCanvas>
+      </BaseLayout>
+    );
+  }
+}
+
+QuestionnairesPage.propTypes = {
   loading: PropTypes.bool,
-  questionnaires: CustomPropTypes.questionnaireList
+  questionnaires: CustomPropTypes.questionnaireList,
+  createQuestionnaire: PropTypes.func.isRequired,
+  onDeleteQuestionnaire: PropTypes.func.isRequired
 };
 
-export default Questionnaires;
+export default QuestionnairesPage;
--- src/containers/QuestionnairesPage/QuestionnairesPage.test.js
+++ src/containers/QuestionnairesPage/QuestionnairesPage.test.js
@@ -1,71 +1,64 @@
 import React from "react";
-import Questionnaires from "./QuestionnairesPage";
+import QuestionnairesPage from "./QuestionnairesPage";
 import { shallow } from "enzyme";
-import shallowWithRouter from "tests/utils/shallowWithRouter";
+import QuestionnaireSettingsModal from "components/QuestionnaireSettingsModal";
 
-describe("containers/Questionnaires", () => {
+describe("containers/QuestionnairesPage", () => {
   const createWrapper = props =>
-    shallowWithRouter(<Questionnaires {...props} />);
+    shallow(
+      <QuestionnairesPage
+        onDeleteQuestionnaire={jest.fn()}
+        createQuestionnaire={jest.fn()}
+        {...props}
+      />
+    );
 
-  let onDeleteQuestionnaire;
-
-  const questionnaire = {
-    id: "1",
-    title: "Test questionnaire",
-    createdAt: "01/01/1970",
-    createdBy: {
-      name: "Mike"
-    },
-    sections: [
-      {
-        id: "5",
-        pages: [
-          {
-            id: "10"
-          }
-        ]
-      }
-    ]
-  };
-
-  beforeEach(() => {
-    onDeleteQuestionnaire = jest.fn();
-  });
-
-  it("should render a 'Create Questionnaire' button", () => {
-    const wrapper = createWrapper({
-      questionnaires: [],
-      onDeleteQuestionnaire
-    });
+  it("should not render table whilst data is loading", () => {
+    const wrapper = createWrapper({ loading: true });
     expect(wrapper).toMatchSnapshot();
   });
 
   it("should render when there are no questionnaires", () => {
-    const props = {
-      questionnaires: [],
-      onDeleteQuestionnaire
-    };
-    const wrapper = shallow(<Questionnaires {...props} />);
+    const wrapper = createWrapper({ questionnaires: [] });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render table when there are questionnaires", () => {
+    const questionnaires = [
+      {
+        id: "1",
+        title: "Test questionnaire",
+        createdAt: "01/01/1970",
+        createdBy: { name: "Mike" },
+        sections: [
+          {
+            id: "5",
+            pages: [{ id: "10" }]
+          }
+        ]
+      }
+    ];
+    const wrapper = createWrapper({ questionnaires });
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should render when there are questionnaires", () => {
-    const props = {
-      questionnaires: [questionnaire],
-      onDeleteQuestionnaire
-    };
-    const wrapper = shallow(<Questionnaires {...props} />);
+  it("allows modal to be open and closed", () => {
+    const wrapper = createWrapper();
 
-    expect(wrapper).toMatchSnapshot();
+    wrapper.find("#btn-create-questionnaire").simulate("click");
+    expect(wrapper.find(QuestionnaireSettingsModal).prop("isOpen")).toBe(true);
+
+    wrapper.find(QuestionnaireSettingsModal).simulate("close");
+    expect(wrapper.find(QuestionnaireSettingsModal).prop("isOpen")).toBe(false);
   });
 
-  it("should not render table whilst data is loading", () => {
-    const props = {
-      loading: true
-    };
-    const wrapper = shallow(<Questionnaires {...props} />);
+  it("creates questionnaire after submission", () => {
+    const createQuestionnaire = jest.fn();
+    const wrapper = createWrapper({ createQuestionnaire });
 
-    expect(wrapper).toMatchSnapshot();
+    wrapper.find(QuestionnaireSettingsModal).simulate("submit");
+
+    expect(createQuestionnaire).toHaveBeenCalled();
   });
 });
