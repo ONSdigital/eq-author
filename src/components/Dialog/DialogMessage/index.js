1ab634446a9db99c65273c6223f339fc0392440b
WickyNilliams <wicky@nillia.ms>
2018-02-07 16:46:42 +0000

    fix invalid element nesting in DeleteConfirmDialog

--- src/components/Dialog/DialogMessage/index.js
+++ src/components/Dialog/DialogMessage/index.js
@@ -26,8 +26,9 @@ export const Subheading = styled.h3`
   margin: 0;
 `;
 
-export const Description = styled.p`
+export const Description = styled.div`
   font-size: 0.9em;
+  margin: 1em 0;
   color: ${textStyle};
 `;
 
