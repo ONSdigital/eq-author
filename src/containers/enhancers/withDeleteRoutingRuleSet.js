800fa321805d955a2113f90a176404bb83274ba7
Samiwel Thomas <samiwel.thomas@gmail.com>
2018-06-06 13:25:37 +0100

    Add enahancer that allows deleting a routing rule set.

---
+++ src/containers/enhancers/withDeleteRoutingRuleSet.js
@@ -0,0 +1,39 @@
+import { graphql } from "react-apollo";
+import deleteRoutingRuleSet from "graphql/deleteRoutingRuleSet.graphql";
+import fragment from "graphql/fragments/page.graphql";
+import { remove } from "lodash";
+
+export const createUpdater = (routingRuleSetId, pageId) => (proxy, result) => {
+  const id = `QuestionPage${pageId}`;
+  const page = proxy.readFragment({
+    id,
+    fragment
+  });
+
+  page.routingRuleSet = null;
+
+  proxy.writeFragment({
+    id,
+    fragment,
+    data: page
+  });
+};
+
+export const mapMutateToProps = ({ mutate, ownProps }) => ({
+  onDeleteRoutingRuleSet(id) {
+    const input = {
+      id
+    };
+
+    const update = createUpdater(id, ownProps.pageId);
+
+    return mutate({
+      variables: { input },
+      update
+    }).then(res => res.data.deleteRoutingRuleSet);
+  }
+});
+
+export default graphql(deleteRoutingRuleSet, {
+  props: mapMutateToProps
+});
