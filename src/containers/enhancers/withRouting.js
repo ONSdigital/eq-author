89f6a85a2bc4f1e605e888d3ae448234d6c5392c
WickyNilliams <wicky@nillia.ms>
2018-06-20 11:03:16 +0100

    use "network-only" as fetchPolicy for withRouting

--- src/containers/enhancers/withRouting.js
+++ src/containers/enhancers/withRouting.js
@@ -2,7 +2,8 @@ import getRoutingQuery from "graphql/getRouting.graphql";
 import { graphql } from "react-apollo";
 
 export const mapPropsToOptions = ({ match: { params } }) => ({
-  variables: params
+  variables: params,
+  fetchPolicy: "network-only"
 });
 
 export default graphql(getRoutingQuery, {
