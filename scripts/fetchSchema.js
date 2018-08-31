fd8638b4cf406a0d1bbbcd4f2dfdfe4375742912
Tom Andrews <git@tomandrews.co.uk>
2018-08-31 14:40:54 +0100

    Fetch the types as well

--- scripts/fetchSchema.js
+++ scripts/fetchSchema.js
@@ -12,7 +12,38 @@ const gql = require("graphql-tag");
 process.env.NODE_ENV = "development";
 require("../config/env");
 const { REACT_APP_API_URL } = process.env;
-const SCHEMA_PATH = path.resolve(__dirname, "..", "schema.json");
+const OUTPUT_PATH = path.resolve(__dirname, "..");
+
+const actionType = process.argv[2];
+
+const actions = {
+  schema: {
+    query: introspectionQuery,
+    file: "schema.json"
+  },
+  fragmentTypes: {
+    query: `
+{
+  __schema {
+    types {
+      kind
+      name
+      possibleTypes {
+        name
+      }
+    }
+  }
+}`,
+    file: "src/apollo/fragmentTypes.json"
+  }
+};
+
+const config = actions[actionType];
+if (!config) {
+  console.error(chalk.red(`Unknown action type: ${actionType}`));
+  process.exit(1);
+}
+const { query, file } = config;
 
 if (!REACT_APP_API_URL) {
   console.error(chalk.red("Error: REACT_APP_API_URL must be set"));
@@ -21,12 +52,13 @@ if (!REACT_APP_API_URL) {
 console.log("Fetching schema from Author API server");
 console.log(`Using Author API Server: ${REACT_APP_API_URL}`);
 
-request(REACT_APP_API_URL, gql(introspectionQuery))
+const filePath = path.join(OUTPUT_PATH, file);
+request(REACT_APP_API_URL, gql(query))
   .then(schema => {
-    fs.writeFileSync(SCHEMA_PATH, JSON.stringify(schema, null, 2));
+    fs.writeFileSync(filePath, JSON.stringify(schema, null, 2));
   })
   .then(() => {
-    console.log(`Wrote file to: ${SCHEMA_PATH}`);
+    console.log(`Wrote file to: ${filePath}`);
   })
   .catch(err => {
     console.error(chalk.red(`Error: ${err.message}`));
