47243a5bdb328b516c110cdede91ada1cf30053c
Tom Andrews <git@tomandrews.co.uk>
2018-10-10 16:16:15 +0100

    Storybook works

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -335,7 +335,13 @@ module.exports = {
             // its runtime that would otherwise be processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|mjs|jsx)$/, /\.html$/, /\.json$/],
+            exclude: [
+              /\.(js|mjs|jsx)$/,
+              /\.html$/,
+              /\.json$/,
+              /\.ejs$/,
+              /\.(graphql|gql)$/
+            ],
             loader: require.resolve("file-loader"),
             options: {
               name: "static/media/[name].[hash:8].[ext]"
