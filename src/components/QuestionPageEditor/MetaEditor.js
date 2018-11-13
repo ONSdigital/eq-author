7423bf31dae8b2efff56ecae6940a3954dd764fe
Hamish Taplin <hello@hamishtaplin.com>
2018-11-13 09:19:09 +0000

    remove dupe prop

--- src/components/QuestionPageEditor/MetaEditor.js
+++ src/components/QuestionPageEditor/MetaEditor.js
@@ -90,7 +90,6 @@ export class StatelessMetaEditor extends React.Component {
           onUpdate={handleUpdate}
           controls={titleControls}
           size="large"
-          placeholder="What is the title?"
           fetchAnswers={fetchAnswers}
           metadata={page.section.questionnaire.metadata}
           testSelector="txt-question-title"
