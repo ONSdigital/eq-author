f9b8022b6fe3b43f73586d5c70fdf1f6d1f120af
Tom Andrews <git@tomandrews.co.uk>
2018-11-19 14:03:50 +0000

    Add tests

--- src/components/PreviewSectionRoute/SectionIntroPreview.js
+++ src/components/PreviewSectionRoute/SectionIntroPreview.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-danger */
+import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
 
@@ -40,5 +41,12 @@ const SectionIntroPreview = ({
     )}
   </Wrapper>
 );
+
+SectionIntroPreview.propTypes = {
+  section: PropTypes.shape({
+    introductionTitle: PropTypes.string,
+    introductionContent: PropTypes.string
+  }).isRequired
+};
 
 export default SectionIntroPreview;
--- src/components/PreviewSectionRoute/index.js
+++ src/components/PreviewSectionRoute/index.js
@@ -1,25 +1,46 @@
+import { propType } from "graphql-anywhere";
 import gql from "graphql-tag";
+import PropTypes from "prop-types";
 import React from "react";
+import { Redirect } from "react-router-dom";
 import { withApollo, Query } from "react-apollo";
 
 import EditorLayout from "components/EditorLayout";
 import Loading from "components/Loading";
 import SectionEditor from "components/SectionEditor";
 
+import { buildDesignPath } from "utils/UrlUtils";
+
 import SectionIntroPreview from "./SectionIntroPreview";
 
-const UnwrappedPreviewSectionRoute = ({ data, loading }) => {
-  if (loading) {
+export const UnwrappedPreviewSectionRoute = ({ match, data, loading }) => {
+  if (loading || !data) {
     return <Loading height="38rem">Preview loading…</Loading>;
   }
 
   const { section } = data;
+
+  if (!section.introductionEnabled) {
+    return <Redirect to={buildDesignPath(match.params)} />;
+  }
 
   return (
     <EditorLayout design preview>
       <SectionIntroPreview section={section} />
     </EditorLayout>
   );
+};
+UnwrappedPreviewSectionRoute.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  data: PropTypes.shape({
+    section: propType(SectionEditor.fragments.Section)
+  }),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      questionnaireId: PropTypes.string.isRequired,
+      sectionId: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 export const SECTION_QUERY = gql`
